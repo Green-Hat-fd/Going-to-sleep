447cbe80e5ffd9b74252577881b2817702b9919b
Green-Hat-fd <108188257+Green-Hat-fd@users.noreply.github.com>
2024-07-05 01:01:39 +0200

    Added InputSystem

---
+++ Proj_GoingToSleep/Assets/Input System/GameManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GameManager : MonoBehaviour
+{
+
+    public InputManager inputManager;
+    public static GameManager inst;
+
+    private void Awake()
+    {
+        inputManager = new InputManager();
+        Time.timeScale = 1f;
+        Application.targetFrameRate = 60;
+    }
+
+    private void OnEnable()
+    {
+        inputManager.Enable();
+        if (inst != null && inst != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            inst = this;
+        }
+    }
+
+    private void OnDisable()
+    {
+        inputManager.Disable();
+    }
+}
